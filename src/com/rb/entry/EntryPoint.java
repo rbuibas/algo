d2e6e4c1633cb5a8b52ea0a88274305203fd3d7d
Raul Buibas <raul.buibas@elektrobit.com>
2017-11-27 17:56:17 +0200

    Initial commit

---
+++ src/com/rb/entry/EntryPoint.java
@@ -0,0 +1,12 @@
+package com.rb.entry;
+
+/**
+ * Entry point for the initial app
+ */
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        System.out.printf("Work it does ?");
+        System.out.println();
+    }
+}
